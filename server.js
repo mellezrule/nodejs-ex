9d160c1b511119b4c68b94920196b0e74a952973
mellezrule <mellezrule@gmail.com>
2017-03-23 13:18:16 -0400

    Update server.js

--- server.js
+++ server.js
@@ -58,7 +58,7 @@ var initDb = function(callback) {
   });
 };
 
-app.get('/', function (req, res) {
+app.get('/payrollisawesome', function (req, res) {
   // try to initialize the db on every request if it's not already
   // initialized.
   if (!db) {
